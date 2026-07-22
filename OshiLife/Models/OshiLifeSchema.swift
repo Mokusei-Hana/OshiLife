ba2ac6f30fbad7d81f5ad8c6b5652703979a58ef
Mokusei-Hana <yukizeroblog@gmail.com>
2026-07-22 20:54:38 +0800

    Make schema version concurrency-safe

--- OshiLife/Models/OshiLifeSchema.swift
+++ OshiLife/Models/OshiLifeSchema.swift
@@ -1,7 +1,7 @@
 import SwiftData
 
 enum OshiLifeSchemaV1: VersionedSchema {
-    static var versionIdentifier = Schema.Version(1, 0, 0)
+    static let versionIdentifier = Schema.Version(1, 0, 0)
     static var models: [any PersistentModel.Type] { [LiveEvent.self] }
 }
 
